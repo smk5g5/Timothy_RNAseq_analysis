30622047ac156f00c10c46d51b9f491be8436881
Saad khan <smk5g5@mail.missouri.edu>
2018-04-24 15:34:34 -0500

    Update pathway_enrichment.R

--- pathway_enrichment.R
+++ pathway_enrichment.R
@@ -2,29 +2,41 @@ library(gtools)
 library(clusterProfiler)
 library(org.Mm.eg.db)
 
-pathway_enrichment <- function(dmr_input,pdf_out){
+pathway_enrichment <- function(dmr_input,tiffout,rdatafile,sig_path){
 annotated_dmr <- read.table(dmr_input,header = T,sep = "\t")
 subset_annot <- annotated_dmr[,c("annot.id","annot.tx_id","annot.gene_id","annot.symbol","meanMethy1","meanMethy2","diff.Methy")]
-subse_annot_omit <- na.omit(subset_annot[,c("annot.gene_id","annot.symbol","meanMethy1","meanMethy2","diff.Methy")])
+subse_annot_omit <- na.omit(subset_annot[,c("annot.gene_id","meanMethy1","meanMethy2","diff.Methy")])
 unique_df <- unique(subse_annot_omit)
-unique_df$fc <- foldchange(unique_df$meanMethy1,unique_df$meanMethy2)
-newdata <- subset(unique_df, fc > 3 | fc < -3)
-ggo <- groupGO(gene     = as.character(unique_df$annot.gene_id),
-OrgDb    = org.Mm.eg.db,
-ont      = "MF",
-level    = 12,
-readable = TRUE)
-pdf(pdf_out)
-barplot(ggo, drop=TRUE, showCategory=12)
-kk <- enrichKEGG(gene = unique_df,
+aggregate_bygene_df <- aggregate(unique_df,by=list(annot.gene_id),FUN = "mean")
+aggregate_bygene_df$fc <- foldchange(aggregate_bygene_df$meanMethy1,aggregate_bygene_df$meanMethy2)
+subset_aggdf <- subset(aggregate_bygene_df[,c("annot.gene_id","meanMethy1","meanMethy2","diff.Methy","fc")], fc > 2 | fc < -2)
+ego <- enrichGO(gene          =  as.character(subset_aggdf$annot.gene_id),
+                universe      = mapped_genes,
+                OrgDb         = org.Mm.eg.db,
+                ont           = "MF",
+                pAdjustMethod = "BH",
+                pvalueCutoff  = 0.01,
+                qvalueCutoff  = 0.05,
+        readable      = TRUE)  
+kk <- enrichKEGG(gene = as.character(subset_aggdf$annot.gene_id),
 organism   = 'mmu',
 pvalueCutoff = 0.05)
-kk <- enrichKEGG(gene = as.character(unique_df$annot.gene_id),
-organism   = 'mmu',
-pvalueCutoff = 0.05)
-barplot(kk)
-  dev.off()
-return(kk)
+tiff_GO <- unlist(paste("Geneontology_",tiffout,sep=''))
+tiff_kegg <- unlist(paste("KEGGenrich_",tiffout,sep=''))
+tiff(tiff_GO, width = 20, height = 10, units = 'in', res = 300)
+print(barplot(ego, drop=TRUE, showCategory=12))
+dev.off()
+tiff(tiff_kegg, width = 10, height = 10, units = 'in', res = 300)
+print(barplot(kk))
+dev.off()
+mykk <- as.data.frame(kk[1:9])
+write.table(x = mykk[mykk$Count>0,],file = sig_path,sep = "\t",row.names = F,col.names=T,quote = F)
+tiff_map <- unlist(paste("enrichmap_",tiffout,sep=''))
+tiff(tiff_map, width = 20, height = 10, units = 'in', res = 300)
+print(enrichMap(ego))
+dev.off()
+save(subset_aggdf,aggregate_bygene_df, ego,kk, file = rdatafile)
+return(ego)
 }
 browseKEGG(kk,'mmu04360')
 genelist <- unique_df$fc
