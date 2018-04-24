7d3a6311a88d5f0decaacead1748090ebdb69843
Saad khan <smk5g5@mail.missouri.edu>
2018-04-24 17:47:58 -0500

    Update pathway_enrichment.R

--- pathway_enrichment.R
+++ pathway_enrichment.R
@@ -1,13 +1,14 @@
 library(gtools)
 library(clusterProfiler)
 library(org.Mm.eg.db)
-
+x <- org.Mm.egCHR
+mapped_genes <- mappedkeys(x)
 pathway_enrichment <- function(dmr_input,tiffout,rdatafile,sig_path){
 annotated_dmr <- read.table(dmr_input,header = T,sep = "\t")
 subset_annot <- annotated_dmr[,c("annot.id","annot.tx_id","annot.gene_id","annot.symbol","meanMethy1","meanMethy2","diff.Methy")]
 subse_annot_omit <- na.omit(subset_annot[,c("annot.gene_id","meanMethy1","meanMethy2","diff.Methy")])
 unique_df <- unique(subse_annot_omit)
-aggregate_bygene_df <- aggregate(unique_df,by=list(annot.gene_id),FUN = "mean")
+aggregate_bygene_df <- aggregate(unique_df,by=list(unique_df$`annot.gene_id`),FUN = "mean")
 aggregate_bygene_df$fc <- foldchange(aggregate_bygene_df$meanMethy1,aggregate_bygene_df$meanMethy2)
 subset_aggdf <- subset(aggregate_bygene_df[,c("annot.gene_id","meanMethy1","meanMethy2","diff.Methy","fc")], fc > 2 | fc < -2)
 ego <- enrichGO(gene          =  as.character(subset_aggdf$annot.gene_id),
@@ -15,7 +16,7 @@ ego <- enrichGO(gene          =  as.character(subset_aggdf$annot.gene_id),
                 OrgDb         = org.Mm.eg.db,
                 ont           = "MF",
                 pAdjustMethod = "BH",
-                pvalueCutoff  = 0.01,
+                pvalueCutoff  = 0.05,
                 qvalueCutoff  = 0.05,
         readable      = TRUE)  
 kk <- enrichKEGG(gene = as.character(subset_aggdf$annot.gene_id),
@@ -28,7 +29,7 @@ print(barplot(ego, drop=TRUE, showCategory=12))
 dev.off()
 tiff(tiff_kegg, width = 10, height = 10, units = 'in', res = 300)
 print(barplot(kk))
-dev.off()
+ dev.off()
 mykk <- as.data.frame(kk[1:9])
 write.table(x = mykk[mykk$Count>0,],file = sig_path,sep = "\t",row.names = F,col.names=T,quote = F)
 tiff_map <- unlist(paste("enrichmap_",tiffout,sep=''))
@@ -38,12 +39,9 @@ dev.off()
 save(subset_aggdf,aggregate_bygene_df, ego,kk, file = rdatafile)
 return(ego)
 }
-browseKEGG(kk,'mmu04360')
+browseKEGG(kk,'mmu05225')
 genelist <- unique_df$fc
 names(genelist) <- as.character(unique_df$annot.gene_id)
-library("pathview")
-source("https://bioconductor.org/biocLite.R")
-biocLite("pathview")
 library(pathview)
 mmu04360 <- pathview(gene.data  = genelist,
 pathway.id = "mmu04360",
@@ -51,10 +49,3 @@ species    = "mmu",
 limit      = list(gene=max(abs(genelist)), cpd=1))
 mmu04360
 dotplot(kk)
-
-
-
-
-
-
-
